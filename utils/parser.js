36d44f8fe98db7f74eafba46eadd04b8f86f50d0
Dhaimohdsub <s202270640.kfupm.sa.edu>
2026-04-01 11:29:30 +0300

    Initialize Node.js project and install lodash

---
+++ utils/parser.js
@@ -0,0 +1,16 @@
+import _ from "lodash";
+
+export function parseNumbers(input) {
+  const numbers = _.map(input, (str) => Number(str));
+
+  
+  _.compact(numbers);
+
+ 
+  return numbers.filter((num) => !Number.isNaN(num));
+}
+
+export function isValidOperation(operation) {
+  const validOps = ["add", "subtract", "multiply", "divide"];
+  return _.includes(validOps, operation);
+}
